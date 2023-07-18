8b324c7ed9cbc143619cfb32c33d3fcc8ee97d1a
Jyotika Patil <jpatil@tesseracthealth.com>
2023-07-18 01:23:30 -0700

    Adde module files

---
+++ custom_module/annotations.py
@@ -0,0 +1,16 @@
+import boto3
+
+def add_custom_annotations():
+    # Add your custom annotation logic here
+    # Initialize the AWS Lambda client
+    # lambda_client = boto3.client('lambda')
+    #
+    # # Invoke the Lambda function
+    # lambda_client.invoke(
+    #     FunctionName='YourLambdaFunctionName',
+    #     InvocationType='Event',
+    #     Payload='{}'
+    # )
+
+    print("Custom annotations added!")
+ 
