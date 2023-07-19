7edd5c43bc055179d0a4fe0959dfe3d75337edb9
Jyotika Patil <jpatil@tesseracthealth.com>
2023-07-19 13:07:35 -0700

    Update config values

--- utils/config.py
+++ utils/config.py
@@ -1,5 +1,5 @@
 DEFAULT_REGION = 'us-east-1'
-bucket = 'identifeye-cloud-ml-export-dev'
-key = '/Users/jyotikapatil/project/custom_annotations_manifold/custom_annotations_manifold/test.csv'
+bucket = 'bucket'
+key = 'key'
 action = 'put_object'
 custom_annotations_lambda = "lambdafunctionname"
